7f198ab74f37af9bc048505b6fcbe0cd89b94112
Ignasi Fosch <natx@y10k.ws>
2015-09-11 20:38:56 +0200

    Add test on process_generic

--- tests/prices/test_aws.py
+++ tests/prices/test_aws.py
@@ -85,12 +85,12 @@ def test_model_ec2(monkeypatch, mock_requests_get, mock_process_model):
 
 def test_model_process(monkeypatch, mock_requests_get, mock_processor):
     sample_urls = {
-        'http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js',
-        'http://a0.awsstatic.com/pricing/1/ec2/rhel-od.min.js',
+        'http://ec2/linux-od.min.js',
+        'http://ec2/rhel-od.min.js',
         }
     sample_content = {
-        'http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js': 'callback({xxx: "xxx"})',
-        'http://a0.awsstatic.com/pricing/1/ec2/rhel-od.min.js': 'callback({yyy: "yyy"})',
+        'http://ec2/linux-od.min.js': 'callback({xxx: "xxx"})',
+        'http://ec2/rhel-od.min.js': 'callback({yyy: "yyy"})',
         }
 
     monkeypatch.setattr('requests.get', mock_requests_get)
@@ -111,3 +111,46 @@ def test_model_process(monkeypatch, mock_requests_get, mock_processor):
     for url in sample_content:
         assert(url in mock_requests_get.urls)
     assert(result == {'xxx': 'xxx', 'yyy': 'yyy'})
+
+def test_model_generic(monkeypatch):
+    data_no_rate = {
+        'vers': "0.1",
+        'config': {
+            'currencies': ['USD'],
+            'regions': [],
+            },
+        }
+    data_rate = {
+        'vers': "0.1",
+        'config': {
+            'rate': 'perh',
+            'currencies': ['USD'],
+            'regions': [],
+            },
+        }
+    sample_content = {
+        'http://ec2/linux-od.min.js': data_no_rate,
+        'http://ec2/rhel-od.min.js': data_rate,
+        }
+
+    monkeypatch.setattr('accloudtant.prices.aws.section_names', {
+        'linux-od.min.js': {
+            'key': 'od',
+            'kind': 'linux',
+            'name': 'On Demand - Linux',
+           },
+        'rhel-od.min.js': {
+            'key': 'od',
+            'kind': 'rhel',
+            'name': 'On Demand - RHEL',
+           },
+        })
+
+    instances = None
+    for url, data in sample_content.items():
+        js_name = url.split('/')[-1]
+        generic, instances = process_generic(data, js_name, instances)
+        assert(generic['version'] == data['vers'])
+        if 'rate' in data['config']:
+            assert(generic['rate'] == data['config']['rate'])
+    assert('linux' in instances and 'rhel' in instances)
