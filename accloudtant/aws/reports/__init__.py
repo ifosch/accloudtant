987e1120de5ad3a410faa02ecbeaeec9d934a557
Ignasi Fosch <natx@y10k.ws>
2015-12-15 23:37:21 +0100

    GH-30: Reports prints the EC2 list

--- accloudtant/aws/reports/__init__.py
+++ accloudtant/aws/reports/__init__.py
@@ -1,8 +1,60 @@
 #!/usr/bin/env python
 import boto3
+from tabulate import tabulate
 
 
 class Reports(object):
     def __init__(self):
         ec2 = boto3.resource('ec2')
         self.instances = list(ec2.instances.all())
+        self.set_billing_os()
+
+    def set_billing_os(self):
+        self.billing_os = {}
+        for instance in self.instances:
+            self.billing_os[instance.id] = self.guess_os(instance)
+
+    def guess_os(self, instance):
+        console_output = instance.console_output()['Output']
+        if 'Windows' in console_output:
+            return ('Windows', 'win')
+        else:
+            if 'RHEL' in console_output:
+                return ('RHEL', 'rhel')
+            elif 'SUSE' in console_output:
+                return ('SUSE', 'suse')
+            elif 'Linux' in console_output:
+                return ('Linux', 'linux')
+            else:
+                return ('Unknown', None)
+
+    def get_name(self, instance):
+        names = [tag for tag in instance.tags if tag['Key'] == 'Name']
+        if names is None:
+            return ''
+        else:
+            return names[0]['Value']
+
+    def __repr__(self):
+        headers = [
+                'Id',
+                'Name',
+                'Type',
+                'AZ',
+                'OS',
+                'State',
+                'Launch time'
+                ]
+        table = []
+        for instance in self.instances:
+            row = [
+                    instance.id,
+                    self.get_name(instance),
+                    instance.instance_type,
+                    instance.placement['AvailabilityZone'],
+                    self.billing_os[instance.id][0],
+                    instance.state['Name'],
+                    instance.launch_time.strftime('%Y-%m-%d %H:%M:%S')
+                    ]
+            table.append(row)
+        return tabulate(table, headers)
--- tests/aws/test_reports.py
+++ tests/aws/test_reports.py
@@ -1,16 +1,55 @@
+import datetime
+from dateutil.tz import tzutc
 import pytest
 import accloudtant.aws.reports
 
 
 @pytest.fixture
 def mock_ec2_resource():
+    class MockEC2Instance(object):
+        def __init__(self, instance):
+            self.instance = instance
+
+        def __eq__(self, obj):
+            if isinstance(obj, dict):
+                return self.id == obj['id']
+            else:
+                return self.id == obj.id
+
+        def console_output(self):
+            return self.instance['console_output']
+
+        @property
+        def id(self):
+            return self.instance['id']
+
+        @property
+        def tags(self):
+            return self.instance['tags']
+
+        @property
+        def instance_type(self):
+            return self.instance['instance_type']
+
+        @property
+        def placement(self):
+            return self.instance['placement']
+
+        @property
+        def state(self):
+            return self.instance['state']
+
+        @property
+        def launch_time(self):
+            return self.instance['launch_time']
+
     class MockEC2Instances(object):
         def __init__(self, instances):
             self.instances = instances
 
         def all(self):
             for instance in self.instances:
-                yield instance
+                yield MockEC2Instance(instance)
 
     class MockEC2Resource(object):
         def __init__(self, responses):
@@ -31,18 +70,112 @@ def mock_ec2_resource():
     return MockEC2ResourceCall()
 
 
-def test_reports(monkeypatch, mock_ec2_resource):
+def test_reports(capsys, monkeypatch, mock_ec2_resource):
     responses = {
         'instances': [{
-            'id': 'i-31a4r567'
+            'id': 'i-912a4392',
+            'tags': [{
+                'Key': 'Name',
+                'Value': 'web1',
+            }, ],
+            'instance_type': 'c3.8xlarge',
+            'placement': {
+                'AvailabilityZone': 'us-east-1c',
+            },
+            'state': {
+                'Name': 'running',
+            },
+            'launch_time': datetime.datetime(
+                    2015,
+                    10,
+                    22,
+                    14,
+                    15,
+                    10,
+                    tzinfo=tzutc(),
+                ),
+            'console_output': {'Output': 'Windows', },
         }, {
-            'id': 'i-9e1p3423'
-        }]
+            'id': 'i-1840273e',
+            'tags': [{
+                'Key': 'Name',
+                'Value': 'app1',
+            }, ],
+            'instance_type': 'r2.8xlarge',
+            'placement': {
+                'AvailabilityZone': 'us-east-1b',
+            },
+            'state': {
+                'Name': 'running',
+            },
+            'launch_time': datetime.datetime(
+                    2015,
+                    10,
+                    22,
+                    14,
+                    15,
+                    10,
+                    tzinfo=tzutc()
+                ),
+            'console_output': {'Output': 'RHEL Linux', },
+        }, {
+            'id': 'i-9840273d',
+            'tags': [{
+                'Key': 'Name',
+                'Value': 'app2',
+            }, ],
+            'instance_type': 'r2.8xlarge',
+            'placement': {
+                'AvailabilityZone': 'us-east-1c',
+            },
+            'state': {
+                'Name': 'stopped',
+            },
+            'launch_time': datetime.datetime(
+                    2015,
+                    10,
+                    22,
+                    14,
+                    15,
+                    10,
+                    tzinfo=tzutc()
+                ),
+            'console_output': {'Output': 'SUSE Linux', },
+        }, {
+            'id': 'i-1840273d',
+            'tags': [{
+                'Key': 'Name',
+                'Value': 'database1',
+            }, ],
+            'instance_type': 'r2.8xlarge',
+            'placement': {
+                'AvailabilityZone': 'us-east-1c',
+            },
+            'state': {
+                'Name': 'stopped',
+            },
+            'launch_time': datetime.datetime(
+                    2015,
+                    10,
+                    22,
+                    14,
+                    15,
+                    10,
+                    tzinfo=tzutc()
+                ),
+            'console_output': {'Output': 'Linux', },
+        }, ]
     }
+    expected = open('tests/aws/report_expected.txt', 'r').read()
 
     monkeypatch.setattr('boto3.resource', mock_ec2_resource)
     mock_ec2_resource.set_responses(responses)
 
     reports = accloudtant.aws.reports.Reports()
+    print(reports)
+    out, err = capsys.readouterr()
 
-    assert (reports.instances == responses['instances'])
+    for mock in responses['instances']:
+        assert([True for reported in reports.instances if reported == mock])
+    print(out)
+    assert (out == expected)
