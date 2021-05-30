d5f036d37c957389f19f120a4225a5ff0fe9c1b5
Ignasi Fosch <natx@y10k.ws>
2021-05-30 07:44:25 +0200

    Identify area based on resource

--- accloudtant/__main__.py
+++ accloudtant/__main__.py
@@ -6,11 +6,13 @@ def area(entry):
         return "EU (Frankfurt)"
 
 
-def get_areas(entries):
+def get_areas(entries, resource_areas):
     areas = {}
 
     for entry in entries:
         area_name = area(entry)
+        if area_name is None and entry[" Resource"] in resource_areas:
+            area_name = resource_areas[entry[" Resource"]]
         if area_name not in areas:
             areas[area_name] = []
         areas[area_name].append(entry)
@@ -20,14 +22,17 @@ def get_areas(entries):
 
 if __name__ == "__main__":
     usage = []
+    resource_areas = {}
 
     with open("tests/fixtures/2021/03/S3.csv") as f:
         reader = csv.DictReader(f)
         for row in reader:
             usage.append(row)
+            if area(row) is not None:
+                resource_areas[row[" Resource"]] = area(row)
 
     print("Simple Storage Service")
-    for area_name, entries in get_areas(usage).items():
+    for area_name, entries in get_areas(usage, resource_areas).items():
         print("\t", area_name)
         for concept in set([entry[" UsageType"] for entry in entries]):
             print("\t\t", concept)
