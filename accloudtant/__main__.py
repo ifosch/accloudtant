9cc5795a4539aeaca4246b2c39fdccdb79307ea9
Ignasi Fosch <natx@y10k.ws>
2021-05-30 12:37:25 +0200

    Move UsageRecord to a separate module

--- accloudtant/__main__.py
+++ accloudtant/__main__.py
@@ -1,33 +1,6 @@
 import csv
 
-
-class UsageRecord(object):
-    def __init__(self, data):
-        self._data = data
-
-    def __getitem__(self, key):
-        return self._data[key]
-
-    @property
-    def type(self):
-        return self._data[" UsageType"]
-
-    @property
-    def value(self):
-        return int(self._data[" UsageValue"])
-
-    @property
-    def resource(self):
-        return self._data[" Resource"]
-
-    @property
-    def area(self):
-        if self.type.startswith("EUC1-"):
-            return "EU (Frankfurt)"
-
-    @property
-    def is_data_transfer(self):
-        return "DataTransfer" in self.type or "CloudFront" in self.type
+from accloudtant.usage_record import UsageRecord
 
 
 def get_areas(entries, resource_areas):
