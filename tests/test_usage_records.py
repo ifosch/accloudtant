8a385f58753b8cf1f0b381ffe03bcda1cb7d14a3
Ignasi Fosch <natx@y10k.ws>
2021-06-06 13:28:30 +0200

    Add test for UsageRecords.totals for data_transfers
    This test validates the totals returned for the corresponding
    data_transfers are correct.

--- tests/test_usage_records.py
+++ tests/test_usage_records.py
@@ -33,3 +33,33 @@ def test_usage_records_totals(csv_file, omit_lambda, expected):
         assert area in expected
         for c, v, u in concepts:
             assert "{} {} {}".format(c, v, u) in expected[area]
+
+
+@pytest.mark.parametrize(
+    "csv_file, omit_lambda, expected",
+    [
+        (
+            "tests/fixtures/2021/03/S3.csv",
+            lambda x: not x.is_data_transfer,
+            {
+                "EU (Frankfurt)": [
+                    "EUC1-DataTransfer-Out-Bytes 0.000 GB",
+                    "EUC1-DataTransfer-In-Bytes 0.010 GB",
+                    "EUC1-CloudFront-Out-Bytes 18.446 GB",
+                ],
+            },
+        ),
+    ],
+)
+def test_usage_records_data_transfers(csv_file, omit_lambda, expected):
+    usage = UsageRecords()
+
+    with open(csv_file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            usage.append(UsageRecord(row))
+
+    for area, concepts in usage.data_transfers().totals(omit=omit_lambda):
+        assert area in expected
+        for c, v, u in concepts:
+            assert "{} {} {}".format(c, v, u) in expected[area]
