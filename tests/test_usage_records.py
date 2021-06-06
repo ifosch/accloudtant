d14fa9cff18ce1fb1fdaa0a5aecf337448c36aa2
Ignasi Fosch <natx@y10k.ws>
2021-06-06 14:04:43 +0200

    Add tests for UsageRecords (#133)
    * Add poetry and pytest
    
    * Add test for UsageRecords.totals
    
    This test validates the returned values for specified input is correct
    as returned by the `totals` method.
    
    * Add test for UsageRecords.totals for data_transfers
    
    This test validates the totals returned for the corresponding
    data_transfers are correct.
    
    * Move CSV file reading logic to load_data function
    
    * Add testing pipeline

---
+++ tests/test_usage_records.py
@@ -0,0 +1,54 @@
+import csv
+import pytest
+
+from accloudtant import load_data
+
+
+@pytest.mark.parametrize(
+    "csv_file, omit_lambda, expected",
+    [
+        (
+            "tests/fixtures/2021/03/S3.csv",
+            lambda x: x.is_data_transfer or x.type == "StorageObjectCount",
+            {
+                "EU (Frankfurt)": [
+                    "EUC1-Requests-Tier1 6.000 Requests",
+                    "EUC1-Requests-Tier2 2861.000 Requests",
+                    "EUC1-TimedStorage-ByteHrs 3.995 GB-Mo",
+                ],
+            },
+        ),
+    ],
+)
+def test_usage_records_totals(csv_file, omit_lambda, expected):
+    usage = load_data(csv_file)
+
+    for area, concepts in usage.totals(omit=omit_lambda):
+        assert area in expected
+        for c, v, u in concepts:
+            assert "{} {} {}".format(c, v, u) in expected[area]
+
+
+@pytest.mark.parametrize(
+    "csv_file, omit_lambda, expected",
+    [
+        (
+            "tests/fixtures/2021/03/S3.csv",
+            lambda x: not x.is_data_transfer,
+            {
+                "EU (Frankfurt)": [
+                    "EUC1-DataTransfer-Out-Bytes 0.000 GB",
+                    "EUC1-DataTransfer-In-Bytes 0.010 GB",
+                    "EUC1-CloudFront-Out-Bytes 18.446 GB",
+                ],
+            },
+        ),
+    ],
+)
+def test_usage_records_data_transfers(csv_file, omit_lambda, expected):
+    usage = load_data(csv_file)
+
+    for area, concepts in usage.data_transfers().totals(omit=omit_lambda):
+        assert area in expected
+        for c, v, u in concepts:
+            assert "{} {} {}".format(c, v, u) in expected[area]
