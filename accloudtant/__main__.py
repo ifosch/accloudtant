f1ff1f923f519987941271ad545ec51c5088275f
Ignasi Fosch <natx@y10k.ws>
2021-06-13 16:49:00 +0200

    Satisfy black

--- accloudtant/__main__.py
+++ accloudtant/__main__.py
@@ -4,8 +4,12 @@ from accloudtant import load_files
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="AWS cost calculator")
-    parser.add_argument("csv_files", type=str, nargs="*",
-                        help='CSV file to read')
+    parser.add_argument(
+        "csv_files",
+        type=str,
+        nargs="*",
+        help="CSV file to read",
+    )
     args = parser.parse_args()
 
     usage = load_files(args.csv_files)
--- accloudtant/aws/cloudfront/__init__.py
+++ accloudtant/aws/cloudfront/__init__.py
@@ -1,9 +1,11 @@
 def omit(usage_record):
-    return is_data_transfer(usage_record.type) \
-        or usage_record.type.endswith("Static") \
-        or usage_record.type.endswith("Dynamic") \
-        or usage_record.type.endswith("Bytes-HTTPS-Proxy") \
+    return (
+        is_data_transfer(usage_record.type)
+        or usage_record.type.endswith("Static")
+        or usage_record.type.endswith("Dynamic")
+        or usage_record.type.endswith("Bytes-HTTPS-Proxy")
         or usage_record.type.endswith("Bytes-HTTP-Proxy")
+    )
 
 
 def is_data_transfer(usage_type):
